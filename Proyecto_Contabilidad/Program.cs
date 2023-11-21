0a5b30d634cbbece188787ed25bec1fb455a020f
Jose Murga <jemurgap@bcds.net>
2023-11-20 23:38:08 -0500

    CRUD Catalogo de Cuentas

--- Proyecto_Contabilidad/Program.cs
+++ Proyecto_Contabilidad/Program.cs
@@ -24,4 +24,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "seat",
+    pattern: "{controller=Seat}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "catalog",
+    pattern: "{controller=Catalog}/{action=Index}/{id?}");
+
 app.Run();
